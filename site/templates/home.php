f0ce158aa94ee21ad882dc14c4b621f36855de44
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-08-15 09:32:01 +0200

    code setup completed

--- site/templates/home.php
+++ site/templates/home.php
@@ -10,13 +10,20 @@
     <!-- STYLESHEETS -->
     <?= css('build/css/style.css') ?>
 
+    <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Murecho:wght@400;500;600;700;800;900&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <h1>PO Starter Kit</h1>
+    <button class="button button-primary">Explore <i class="anchor-first fa fa-chevron-right" aria-hidden="true"></i></button>
+    <button class="button button-secondary">Explore</button>
+    <button class="button button-white">Explore</button>
 </body>
 
 </html>
