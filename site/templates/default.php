1692a9bd925146fd1a076494b4ae84583032b6be
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-08-17 11:42:14 +0200

    added default styling

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,9 +3,17 @@
 <!-- CONTAINER ERROR -->
 <div id="container" class="container container-error">
 
+    <!-- NAV -->
+    <?php snippet('general/nav') ?>
+
+
+
     <!-- ERROR-CONTAINER -->
     <section class="error-content">
-        <!-- CONTENT -->
+        <h1>Oooppsss</h1>
+        <p>Het is al geruime tijd een bekend gegeven dat een lezer, tijdens het bekijken.</p>
+
+        <a class="button-primary" href="<?= $site->url() ?>">Home <i class="anchor-first fa fa-chevron-right" aria-hidden="true"></i></a>
     </section>
 </div>
 
