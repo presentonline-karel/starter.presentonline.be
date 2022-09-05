b5c44c17a15d71183b7db6f8dffcfd3e4ba4e260
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-09-05 16:14:39 +0200

    contactform done?

--- site/templates/emails/email.html.php
+++ site/templates/emails/email.html.php
@@ -1,6 +1,6 @@
 Het contactformulier werd ingevuld door <b><?= $name ?></b><br><br>
 
-<b>Email</b> <?= $email ?><br>
-<b>Onderwerp</b> <?= $subject ?><br>
+<b>Email -</b> <?= $email ?><br>
+<b>Onderwerp -</b> <?= $subject ?><br>
 
 <b>Boodschap</b><br><br> <?= $message ?>
