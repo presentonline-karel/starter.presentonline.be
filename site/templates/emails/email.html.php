81619697ab14d994e9547efa0fa555baad749ec7
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-09-05 16:04:46 +0200

    changed email template slightly

--- site/templates/emails/email.html.php
+++ site/templates/emails/email.html.php
@@ -1,5 +1,6 @@
 Het contactformulier werd ingevuld door <b><?= $name ?></b><br><br>
 
 <b>Email</b> <?= $email ?><br>
+<b>Onderwerp</b> <?= $subject ?><br>
 
-<b>Boodschap</b> <?= $message ?><br>
+<b>Boodschap</b><br><br> <?= $message ?>
