a706ee42f0c2fc9c69b931e455adcc83fd8ac096
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2023-02-10 17:47:47 +0100

    Created seperate roles for editor & admin

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
-require 'kirby/bootstrap.php';
+//require "kirby/bootstrap.php";
+//echo (new Kirby)->render();
 
-echo (new Kirby)->render();
+
+
+require __DIR__ . '/kirby/bootstrap.php';
+
+$kirby = new Kirby();
+
+$currentUser = $kirby->user();
+
+if ( $currentUser && $currentUser->role()->name() === 'editor' ):
+   $kirby = new Kirby([
+       'roots' => [
+           'blueprints' => __DIR__ . '/site/blueprints-editor',
+       ],
+   ]);
+
+elseif ( $currentUser && $currentUser->role()->name() === 'admin' ):
+   $kirby = new Kirby([
+       'roots' => [
+           'blueprints' => __DIR__ . '/site/blueprints',
+       ],
+   ]);
+endif;
+
+echo $kirby->render();
